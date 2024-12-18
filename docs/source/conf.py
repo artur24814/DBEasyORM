43cd6bf58ef126e4f0ba5770c6835ef6df882a8b
Artur Yurchenko <artur24814@gmail.com>
2024-12-18 14:37:57 +0100

    The Sphinx docs generator has been initialized

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+project = 'DBEasyORM'
+copyright = '2024, artur24814'
+author = 'artur24814'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary'
+]
+napoleon_google_docstring = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Base Dir Location -----------------------------------------------------------------
+sys.path.insert(0, os.path.abspath('../..'))
+print("sys.path during Sphinx build:", sys.path)
